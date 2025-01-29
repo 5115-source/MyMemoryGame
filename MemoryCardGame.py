d84a2919ea3bf6167e277fff0ecca0025f8aa6de
5115-Source <chegstad5115@student.whatcom.edu>
2025-01-28 17:31:10 -0800

    Finished reworking the entire progam.

--- MemoryCardGame.py
+++ MemoryCardGame.py
@@ -1,22 +1,27 @@
+#ChatGPT helped with game introduction and understanding python formatting and some linked list problem solving
+#Used chatGPT to help with learning python syntax and how to arrange pieces for a desired output
+#Used geeks for geeks to reference some python linked list base code
 import random
 
+#Node class to use in creating and manipulating linked lists
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-# Create a LinkedList class
+#linked list class that has a few general linked list manipulation methods
+#Also has more purpose built custom methods
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    # Method to add a node at the beginning of the linked list
+    #Method to add a node at the beginning of the linked list
     def insertAtBegin(self, data):
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
 
-    # Update node at a given position
+    #Update node at a given position in the linked list
     def updateNode(self, val, index):
         current_node = self.head
         position = 0
@@ -29,7 +34,7 @@ class LinkedList:
         else:
             print("Index not present")
             
-    # Print the formatted deck linked list
+    #Print the formatted deck linked list as a game board
     def printBoard(self):
         current_node = self.head
         nodeCounter = 0
@@ -46,14 +51,14 @@ class LinkedList:
                     print(f"{current_node.data[1]}", end = "  ")
                 else:
                     print(f" {current_node.data[1]}", end = "  ")
-            #If the data is a symbol (not an int)
-            else: 
+            else: #If the data is a symbol (not an int)
                 print(f" {current_node.data[0]}", end = "  ")
                 
             current_node = current_node.next
             nodeCounter += 1
         print("\n")
         
+    #Check if ALL cards are symbols (not ints)
     def allNonInt(self):
         current_node = self.head
         position = 0
@@ -69,7 +74,7 @@ class LinkedList:
         else:
             return False
 
-# Print the linked list for troubleshooting
+    #Print the linked list (used for troubleshooting)
     def printLL(self):
         current_node = self.head
         while current_node:
@@ -78,7 +83,6 @@ class LinkedList:
         print()
 
 def main():
-    #ChatGPT helped with instructions and understanding python formatting and some linked list problem solving
     print("The memory card game, also known as Concentration, is a card-matching game where all cards are laid face down.")
     print("You will take turns flipping over two cards at a time, trying to find matching pairs.")
     print("The goal is to remember the locations of cards to match them and collect pairs until all cards are matched.")
@@ -88,24 +92,22 @@ def main():
     gameRunning = True
 
     while gameRunning == True:
-        deck.printBoard()
-        #Print the linked list for troubleshooting
-        deck.printLL()
+        #Uncomment to print the linked list for troubleshooting or "cheating" the game
+        #deck.printLL()
         
+        deck.printBoard()
         guessedCards_guessedSymbols = inputChecker(deck)
         
         if isItAPair(guessedCards_guessedSymbols) == True:
             deck.updateNode(guessedCards_guessedSymbols[1][0], int(guessedCards_guessedSymbols[0][0]) - 1)
             deck.updateNode(guessedCards_guessedSymbols[1][1], int(guessedCards_guessedSymbols[0][1]) - 1)
 
-        #deck.allNonInt()
-
         if deck.allNonInt() == True:
             deck.printBoard()
             print("Congratulations!!! You beat the game. Good job sticking in there!")
             gameRunning = False
     
-#Build a "shuffled" stack of 20 cards that has 2 of each symbol in it in it
+#Build a "shuffled" stack of 20 cards that has 2 of each symbol and a number on the "back" of each card.
 def deckBuilder():
     deck = LinkedList()
     
@@ -122,32 +124,32 @@ def deckBuilder():
 
     return deck
         
+#Take user input and make sure it is valid. Then check what the symbols on the cards are and return the user chosen cards and symbols.
 def inputChecker(deck):
     cardsToFlip = [0, 0]
     cardSymbols = [0, 1]
     
     for i in range(0, 2):
         cardsToFlip[i] = input("What is the number of the next card you want to flip? ")
-        while not cardsToFlip[i].isdigit() or not 1 <= int(cardsToFlip[i]) <= 20: #Could add a case where the number has already been guessed and is a symbol
+        while not cardsToFlip[i].isdigit() or not 1 <= int(cardsToFlip[i]) <= 20:
             cardsToFlip[i] = input("Sorry that wasn't an integer number between 1-20. What is the number (1-20) of the next card you want to flip? ")
     
         current = deck.head
-        nodeCounter = 1  # Counter to keep track of the current index
+        nodeCounter = 1
             
         while current:
             if nodeCounter == int(cardsToFlip[i]):
-                cardSymbols[i] = current.data[0]  # Return the data at the index
+                cardSymbols[i] = current.data[0]
             current = current.next
             nodeCounter += 1
             
         print("Card", cardsToFlip[i], "is: ", cardSymbols[i])
             
     guessedCards_guessedSymbols = [cardsToFlip, cardSymbols]
-    
     return guessedCards_guessedSymbols
 
+#If the symbols on the cards are the same then they form a pair
 def isItAPair(guessedCards_guessedSymbols):
-    #If the symbols on the cards are the same then they form a pair
     if guessedCards_guessedSymbols[1][0] == guessedCards_guessedSymbols[1][1]:
         print("Card", guessedCards_guessedSymbols[0][0], "and card", guessedCards_guessedSymbols[0][1], "are a pair!")
         return True
